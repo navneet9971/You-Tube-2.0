d5200459945111c6cf69e01568c5d40a048c9263
unknown <Navneetjha2012@gmail.com>
2022-09-26 01:01:42 +0530

    Added my project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+
 
 
 import { logo } from "../utils/constants";
@@ -10,12 +12,12 @@ const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} /> 
-      <span style={{fontFamily: "fantasy" ,fontSize: "27px", color: "red", inlineSize: "48px" }}>YOU
-      </span>
-      <span style={{fontFamily: "fantasy" , fontSize: "27px", color: "aqua", inlineSize: "60px" }}>TUBE
-      </span>
-      <span style={{fontFamily: "fantasy" ,fontSize: "27px", color: "white" }}>2.0
-      </span>
+      
+<Box>
+  <Typography variant="h5" fontWeight= "bold" display='flex' sx={{ color: 'red' }}>
+    You<span style= {{ color: "aqua"  }}>Tube</span><span style= {{ color: 'white',}}>2.0</span>
+  </Typography>
+</Box>
       
       
 
